d2e666a6674481e647b1f0817756a37ee07a7ca4
storm5030 <storm5030@gmail.com>
2025-12-24 16:14:40 +0900

    first commit

---
+++ avatar_bringup/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'avatar_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+
+        ('share/' + package_name, ['package.xml']),
+        
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        
+        (os.path.join('share', package_name, 'config/follower'), glob('config/follower/*.yaml')),
+        (os.path.join('share', package_name, 'config/leader'), glob('config/leader/*.yaml')),
+        (os.path.join('share', package_name, 'config/vision'), glob('config/vision/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Dongryun',
+    maintainer_email='storm5030@gmail.com',
+    description='Bringup package for Avatar Robot (Python)',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'joint_trajectory_executor = avatar_bringup.joint_trajectory_executor:main',
+        ],
+    },
+)
