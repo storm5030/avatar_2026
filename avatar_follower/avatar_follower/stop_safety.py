061590f5d8249a69285fa46e7f734e2a3c91aa95
ss9000b <ss9000b@gmail.com>
2026-02-10 06:27:14 +0900

    충돌감지 오류 추가 수정(아직 수정 필요)

--- avatar_follower/avatar_follower/stop_safety.py
+++ avatar_follower/avatar_follower/stop_safety.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from ros_gz_interfaces.msg import Contacts
-from std_msgs.msg import Float64MultiArray
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
 
 class SafetyStopNode(Node):
     def __init__(self):
@@ -15,19 +16,44 @@ class SafetyStopNode(Node):
             '/world/default/model/follower/link/left_link_gripper_1/sensor/left_gripper_bumper/contact',
             self.collision_callback,
             10)
+        
+        self.sub_right = self.create_subscription(
+            Contacts,
+            '/world/default/model/follower/link/right_link_gripper_1/sensor/right_gripper_bumper/contact', # 오른손 긴 주소 (확인 필요!)
+            self.collision_callback,
+            10)
             
         # 2. 모터 컨트롤러 퍼블리셔 (멈춤 명령용)
         # (사용하시는 컨트롤러 토픽 이름으로 바꾸세요!)
         self.publisher_ = self.create_publisher(
-            Float64MultiArray,
-            '/forward_velocity_controller/commands', 
+            JointTrajectory,
+            '/arm_controller/joint_trajectory', 
             10)
-
+        
         self.is_stopped = False
 
+        self.joint_names = [
+            'right_joint1',
+            'right_joint2', 
+            'right_joint3',
+            'right_joint4',
+            'right_joint5',
+            'right_joint6',
+            'right_joint_gripper',
+            'left_joint1',
+            'left_joint2',
+            'left_joint3',
+            'left_joint4',
+            'left_joint5',
+            'left_joint6',
+            'left_joint_gripper',
+            'neck_joint1',
+            'neck_joint2'
+        ]
+
     def collision_callback(self, msg):
-        # msg.states 리스트가 비어있지 않으면 충돌 중이라는 뜻!
-        if len(msg.states) > 0:
+        # msg.contacts 리스트가 비어있지 않으면 충돌 중이라는 뜻!
+        if len(msg.contacts) > 0:
             if not self.is_stopped:
                 self.get_logger().warn('충돌 발생! 로봇 정지!')
                 self.stop_robot()
@@ -36,11 +62,17 @@ class SafetyStopNode(Node):
             self.is_stopped = False
 
     def stop_robot(self):
-        stop_msg = Float64MultiArray()
-        # 두 팔 로봇이라면 0.0을 더 많이 넣어야겠죠?
-        # 예: [왼쪽1, ..., 왼쪽6, 오른쪽1, ..., 오른쪽6]
-        stop_msg.data = [0.0] * 16  # 관절이 16개라면 이렇게!
-        self.publisher_.publish(stop_msg)
+        stop_msg = JointTrajectory()
+        stop_msg.joint_names = self.joint_names
+
+        point = JointTrajectoryPoint()
+        point.positions = [0.0] * len(self.joint_names) # 현재 위치를 유지하려면 별도 로직 필요
+        point.velocities = [0.0] * len(self.joint_names) # 속도를 0으로!
+        point.time_from_start.sec = 0
+        point.time_from_start.nanosec = 100000000 # 0.1초 안에 멈춰라
+
+        stop_msg.points.append(point)
+        self.publisher_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
